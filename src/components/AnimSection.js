5226f33a3f1e4b936312b31ff4af0ac500a5e740
richipineda <richipineda2@gmail.com>
2022-02-28 20:23:07 +0000

    fixed height

--- src/components/AnimSection.js
+++ src/components/AnimSection.js
@@ -2,12 +2,12 @@ import React from 'react'
 
 function AnimSection() {
     return (
-        <div className='bg-secondary h-screen flex flex-col relative font-Poppins-Medium'>
-            <div className='w-full h-1/4 bg-printed-primary bg-center absolute top-0' />
-            <div className='h-screen flex flex-col justify-center items-center'>
-                <p className='text-white text-left text-4xl font-bold w-1/2'>Web 3.0 has taken off, and its full of new possibillities and ideas.</p>
+        <div className='relative flex flex-col h-screen bg-secondary font-Poppins-Medium'>
+            <div className='absolute top-0 flex w-full bg-center h-1/6 md:h-1/4 bg-printed-primary' />
+            <div className='flex flex-col items-center justify-center h-screen'>
+                <p className='w-1/2 text-4xl font-bold text-left text-white'>Web 3.0 has taken off, and its full of new possibillities and ideas.</p>
             </div>
-            <div className='w-full h-1/4 bg-printed-primary bg-center absolute bottom-0' />
+            <div className='absolute bottom-0 flex w-full bg-center h-1/6 md:h-1/4 bg-printed-primary' />
         </div>
     )
 }
