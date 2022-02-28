4847b9f4d543eeae18956cf56ca4b95342ac6499
richipineda <richipineda2@gmail.com>
2022-02-28 01:21:46 +0000

    responsive fixes

--- src/components/FormSection.js
+++ src/components/FormSection.js
@@ -32,8 +32,8 @@ function FormSection() {
                                     transition
                                     ease-in-out
                                     m-0
-                                    focus:text-gray-700 focus:bg-white focus:border-blue-600 focus:outline-none" id="exampleInput123"
-                                    aria-describedby="emailHelp123" placeholder="Name" />
+                                    focus:text-gray-700 focus:bg-white focus:border-blue-600 focus:outline-none" id=""
+                                    aria-describedby="" placeholder="Name" />
                             </div>
                             <div class="form-group mb-6">
                                 <input type="text" class="
@@ -49,8 +49,9 @@ function FormSection() {
                                        transition
                                        ease-in-out
                                        m-0
-                                       focus:text-gray-700 focus:bg-white focus:border-blue-600 focus:outline-none" id="exampleInput124"
-                                    aria-describedby="emailHelp124" placeholder="Email" />
+                                       focus:text-gray-700 focus:bg-white focus:border-blue-600 focus:outline-none"
+                                    id=""
+                                    aria-describedby="" placeholder="Email" />
                             </div>
                         </div>
                         <div class="mb-6">
@@ -68,8 +69,8 @@ function FormSection() {
                                     m-0
                                     focus:text-gray-700 focus:bg-white focus:border-blue-600 focus:outline-none
                                 "
-                                id="exampleFormControlTextarea13"
-                                rows="3"
+                                id=""
+                                rows="4"
                                 placeholder="Message"
                             ></textarea>
                         </div>
@@ -88,10 +89,10 @@ function FormSection() {
                                 checked:bg-blue-600
                                 mr-2
                                 cursor-pointer"
-                                    id="exampleCheck87" />
-                                <label class="form-check-label inline-block text-white sm:text-sm shrink" for="exampleCheck87">Get a reply on Telegram</label>
+                                    id="" />
+                                <label class="form-check-label inline-block text-white" for="">Get a reply on Telegram</label>
                             </div>
-                            <div className='flex flex-row justify-around w-1/2 m-4 md:w-1/4 md:justify-end'>
+                            <div className='flex flex-row justify-around w-1/2 mt-4 md:mt-0 md:w-1/4 md:justify-end'>
                                 <button type='submit' className='w-10 h-10 border-2 border-gray-300 border-solid rounded-full hover:border-blue-500 md:mx-4'><PaperClip /></button>
                                 <button type='submit' className='w-10 h-10 border-2 border-gray-300 border-solid rounded-full hover:border-blue-500'><ArrowRight /></button>
                             </div>
--- src/components/ImgBubble.js
+++ src/components/ImgBubble.js
@@ -3,7 +3,6 @@ import React from 'react'
 function ImgBubble() {
     return (
         <div className='w-12 h-12 bg-gray-900'>
-            <h1>Hola</h1>
         </div>
     )
 }
