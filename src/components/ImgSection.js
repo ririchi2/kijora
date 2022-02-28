7fd224eb776578e4e6a10d828698046b469a723a
richipineda <richipineda2@gmail.com>
2022-02-28 12:43:59 +0000

    overflow fix and md:text

--- src/components/ImgSection.js
+++ src/components/ImgSection.js
@@ -6,9 +6,9 @@ function ImgSection() {
         <div className='relative h-screen text-white bg-terciary'>
             {/* <div className='h-32 mb-5 bg-printed-terciary' /> */}
             <div className='absolute top-0 w-full bg-center h-1/6 bg-printed-terciary' />
-            <div className='flex flex-col items-center justify-center w-full h-full sm:bg-primary'>
+            <div className='flex flex-col items-center justify-center w-full sm:bg-primary'>
                 <p className='text-gray-500 mt-28 font-Poppins-Regular'>Services</p>
-                <h1 className='w-1/2 my-5 text-4xl font-bold text-center text-white font-Poppins-Medium'>Here's what we do</h1>
+                <h1 className='w-1/2 my-5 text-3xl font-bold text-center text-white md:text-4xl font-Poppins-Medium'>Here's what we do</h1>
                 <div className='grid w-1/2 grid-cols-2 gap-4 h-1/2'>
                     <div className='col-span-2 img'></div>
                     <div className='img'></div>
